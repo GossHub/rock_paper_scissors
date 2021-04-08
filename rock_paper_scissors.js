69a40d8a511ee0c874f7a450d93bc47f6b08f685
DESKTOP-RAAT022\Ayan <tuna.ayan@pubinno.com>
2021-04-08 20:52:56 +0200

    first commit

---
+++ rock_paper_scissors.js
@@ -0,0 +1,24 @@
+const rspList = ['Rock', 'Paper', 'Scissors'];
+
+let gossHand = rspList[(Math.floor(Math.random() * 10))%3];
+let tunnHand = rspList[(Math.floor(Math.random() * 10))%3];
+
+
+console.log(gossHand);
+console.log(tunnHand);
+
+if (gossHand ==='Rock' && tunnHand === 'Paper') {
+    console.log('Tuna Win!');
+} else if (gossHand ==='Rock' && tunnHand === 'Scissors') {
+    console.log('Gözde Win!');
+} else if (gossHand ==='Paper' && tunnHand === 'Rock') {
+    console.log('Gözde Win!');
+} else if (gossHand ==='Paper' && tunnHand === 'Scissors') {
+    console.log('Tuna Win!');
+} else if (gossHand ==='Scissors' && tunnHand === 'Rock') {
+    console.log('Tuna Win!');
+} else if (gossHand ==='Scissors' && tunnHand === 'Paper') {
+    console.log('Gözde Win!');
+} else {
+    console.log('Even!');
+}
